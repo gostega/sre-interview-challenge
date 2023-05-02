d3030eb9689c1fb2fc0f6342d47d0ece50113794
James Tuson <gostega@users.noreply.github.com>
2023-05-02 18:05:30 +0800

    Apply prettier formatting suggestions

--- simplewebsite/assets/main.js
+++ simplewebsite/assets/main.js
@@ -1,7 +1,7 @@
 // DOM Elements
-const time = document.getElementById('time'),
-  greeting = document.getElementById('greeting'),
-  sourcecode = document.getElementById('sourcecode');
+const time = document.getElementById("time"),
+  greeting = document.getElementById("greeting"),
+  sourcecode = document.getElementById("sourcecode");
 
 // Set Background and Greeting
 function loadPage() {
@@ -10,19 +10,22 @@ function loadPage() {
 
   if (hour < 12) {
     // Morning
-    document.body.style.backgroundImage = "url('./assets/perth-koondoola-morning.jpg')";
-    greeting.textContent = 'Good Morning';
-    document.body.style.color = 'white';
+    document.body.style.backgroundImage =
+      "url('./assets/perth-koondoola-morning.jpg')";
+    greeting.textContent = "Good Morning";
+    document.body.style.color = "white";
   } else if (hour < 18) {
     // Afternoon
-    document.body.style.backgroundImage = "url('./assets/perth-scarborough-afternoon.jpg')";
-    greeting.textContent = 'Good Afternoon';
-    document.body.style.color = 'white';
+    document.body.style.backgroundImage =
+      "url('./assets/perth-scarborough-afternoon.jpg')";
+    greeting.textContent = "Good Afternoon";
+    document.body.style.color = "white";
   } else {
     // Evening
-    document.body.style.backgroundImage = "url('./assets/perth-koondoola-evening.jpg')";
-    greeting.textContent = 'Good Evening';
-    document.body.style.color = 'white';
+    document.body.style.backgroundImage =
+      "url('./assets/perth-koondoola-evening.jpg')";
+    greeting.textContent = "Good Evening";
+    document.body.style.color = "white";
   }
 }
 
